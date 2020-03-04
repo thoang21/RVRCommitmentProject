1e201e8a88a7e8c4638d622a26a0639d6701842c
JMecham <49049918+JMecham@users.noreply.github.com>
2020-03-04 08:28:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -22,13 +22,100 @@ I will be syncing with this repository on my computer and pasting the code into 
 */
 
 async function startProgram() {
-	await stageOne()
-	await stageTwo()
-	await stageThree()
+	//Jake' Movement Here (Stage 1):
+	
+	//Jaden's Movement Here (Stage 2):
+	await stageTwoPart1()
+	await stageTwoPart2()
+	await stageTwoPart3()
+	await stageTwoPart4()
+	
+	//Other People's Movement Here:
 	exitProgram()
 }
 
-async function stageOne(){
+
+async function stageTwoPart1(){
+
+//This is a hacky way to quickly do a point turn to 90 degrees.
+
+//This function rolls the motors at a heading of 90, with a motor speed of 50, for 0.1 seconds.
+await roll(90,50,0.1)
+//...and then this moves it back.
+await roll(90,-50,0.1)
+}
+
+async function stageTwoPart2(){
+
+
+	let setpoint = 120;
+	let k = 2.0;
+	let kD = 0.5;
+	let kI = 0.001;
+	var accumulatedError = 0;
+	var oldError = 0;
+	var successTimer = 0.0;
+	var maxSpeed = 100;
+
+	var stageComplete = false;
+
+	//Visual feedback to know which stage we are on
+	await setMainLed({r:255,g:0,b:0})
+
+	while(stageComplete != true){
+		//get the current location of the robot.
+		var location = getLocation().y;
+
+		//Use a PID algorithm to set the position of the robot
+		var error = setpoint - location;
+		var changeError = error - oldError;
+		accumulatedError = error + accumulatedError
+
+
+		var output = k*error - kD*changeError + kI*accumulatedError;
+		oldError = error
+
+		if(output >maxSpeed){
+
+			output = maxSpeed;
+
+		}
+		if(output < -255){
+
+			output = -maxSpeed;
+		}
+
+		//This function rolls the motors at a heading of 0, with a motor speed of output, for 0.2 seconds.
+		await roll(0,output,0.2);
+
+		if(error < 2.0){
+			successTimer += 0.1;
+
+		}
+
+		//If the error has been less than 2.0 cm for more than half a second, finish the stage.
+		if(successTimer > 0.5){
+			stageComplete = true
+		}
+
+		await delay(0.025);
+		//If our error is less than 1.0 cm, keep track of how long that has been the case.
+
+	}
+
+}
+
+async function stageTwoPart3(){
+
+//This is a hacky way to quickly do a point turn to 90 degrees.
+
+//This function rolls the motors at a heading of 90, with a motor speed of 50, for 0.1 seconds.
+await roll(90,-50,0.1)
+//...and then this moves it back.
+await roll(90,50,0.1)
+}
+
+async function stageTwoPart4(){
 
 
 	let setpoint = 180;
@@ -88,69 +175,3 @@ async function stageOne(){
 
 }
 
-async function stageTwo(){
-
-//This is a hacky way to quickly do a point turn to 90 degrees.
-
-//This function rolls the motors at a heading of 90, with a motor speed of 50, for 0.1 seconds.
-await roll(90,50,0.1)
-//...and then this moves it back.
-await roll(90,-50,0.1)
-}
-
-
-async function stageThree(){
-
-	//Travel for eighty centimeters at a heading of 90 degrees
-
-	let setpoint = 120;
-	let k = 2.0;
-	let kD = 0.5;
-	let kI = 0.001;
-	var accumulatedError = 0;
-	var oldError = 0;
-	var successTimer = 0.0;
-	var maxSpeed = 100;
-
-	stageComplete = false;
-
-	await setMainLed({r:0,g:0,b:255})
-
-	while(stageComplete != true){
-		//get the current location of the robot. Note that this uses the x-coordinate this time, not y.
-		var location = getLocation().x;
-
-		//Use a PID algorithm to set the position of the robot
-		var error = setpoint - location;
-		var changeError = error - oldError;
-		accumulatedError = error + accumulatedError
-
-
-		var output = k*error - kD*changeError + kI*accumulatedError;
-		oldError = error
-
-		if(output > maxSpeed){
-
-			output = maxSpeed;
-
-		}
-		if(output < -maxSpeed){
-
-			output = -maxSpeed;
-		}
-		//We want all of this to be happening at a heading of 90 degrees
-
-		await roll(90,output,0.2);
-
-		await delay(0.025);
-		//If our error is less than 2.0 cm, keep track of how long that has been the case.
-		if(error < 2.0){
-			successTimer += 0.025;
-		}
-		//If the error has been less than 2.0 cm for more than half a second, finish the stage.
-		if(successTimer > 0.5){
-			stageComplete = true
-		}
-	}
-
-}
