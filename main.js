e7e0dc8c0ff28e89192cdcdca566a8d66ad80111
Evan Weinberg <eweinberg@ssis.edu.vn>
2020-03-04 08:51:04 +0700

    Update main.js
    Consolidate Jake and Jaden's stages into main code.
    
    This sets up a naming convention we can use moving forward for stages.

--- main.js
+++ main.js
@@ -23,19 +23,146 @@ I will be syncing with this repository on my computer and pasting the code into 
 
 async function startProgram() {
 	//Jake' Movement Here (Stage 1):
-	
+	await JakeStage1()
+	await JakeStage2()
+	await JakeStage3()
 	//Jaden's Movement Here (Stage 2):
-	await stageTwoPart1()
-	await stageTwoPart2()
-	await stageTwoPart3()
-	await stageTwoPart4()
+	await JadenStage1()
+	await JadenStage2()
+	await JadenStage3()
+	await JadenStage4()
 	
 	//Other People's Movement Here:
 	exitProgram()
 }
 
+async function JakeStage1(){
 
-async function stageTwoPart1(){
+
+	let setpoint = 180;
+	let k = 2.0;
+	let kD = 0.5;
+	let kI = 0.001;
+	var accumulatedError = 0;
+	var oldError = 0;
+	var successTimer = 0.0;
+	var maxSpeed = 100;
+
+	var stageComplete = false;
+
+	//Visual feedback to know which stage we are on
+	await setMainLed({r:255,g:0,b:0})
+
+	while(stageComplete != true){
+		//get the current location of the robot.
+		var location = getLocation().y;
+
+		//Use a PID algorithm to set the position of the robot
+		var error = setpoint - location;
+		var changeError = error - oldError;
+		accumulatedError = error + accumulatedError
+
+
+		var output = k*error - kD*changeError + kI*accumulatedError;
+		oldError = error
+
+		if(output >maxSpeed){
+
+			output = maxSpeed;
+
+		}
+		if(output < -255){
+
+			output = -maxSpeed;
+		}
+
+		//This function rolls the motors at a heading of 0, with a motor speed of output, for 0.2 seconds.
+		await roll(0,output,0.2);
+
+		if(error < 2.0){
+			successTimer += 0.1;
+
+		}
+
+		//If the error has been less than 2.0 cm for more than half a second, finish the stage.
+		if(successTimer > 0.5){
+			stageComplete = true
+		}
+
+		await delay(0.025);
+		//If our error is less than 1.0 cm, keep track of how long that has been the case.
+
+	}
+
+}
+
+async function JakeStage2(){
+
+//This is a hacky way to quickly do a point turn to 90 degrees.
+
+//This function rolls the motors at a heading of 90, with a motor speed of 50, for 0.1 seconds.
+await roll(90,50,0.1)
+//...and then this moves it back.
+await roll(90,-50,0.1)
+}
+
+
+async function JakeStage3(){
+
+	//Travel for eighty centimeters at a heading of 90 degrees
+
+	let setpoint = 120;
+	let k = 2.0;
+	let kD = 0.5;
+	let kI = 0.001;
+	var accumulatedError = 0;
+	var oldError = 0;
+	var successTimer = 0.0;
+	var maxSpeed = 100;
+
+	stageComplete = false;
+
+	await setMainLed({r:0,g:0,b:255})
+
+	while(stageComplete != true){
+		//get the current location of the robot. Note that this uses the x-coordinate this time, not y.
+		var location = getLocation().x;
+
+		//Use a PID algorithm to set the position of the robot
+		var error = setpoint - location;
+		var changeError = error - oldError;
+		accumulatedError = error + accumulatedError
+
+
+		var output = k*error - kD*changeError + kI*accumulatedError;
+		oldError = error
+
+		if(output > maxSpeed){
+
+			output = maxSpeed;
+
+		}
+		if(output < -maxSpeed){
+
+			output = -maxSpeed;
+		}
+		//We want all of this to be happening at a heading of 90 degrees
+
+		await roll(90,output,0.2);
+
+		await delay(0.025);
+		//If our error is less than 2.0 cm, keep track of how long that has been the case.
+		if(error < 2.0){
+			successTimer += 0.025;
+		}
+		//If the error has been less than 2.0 cm for more than half a second, finish the stage.
+		if(successTimer > 0.5){
+			stageComplete = true
+		}
+	}
+
+}
+async function JadenStage1(){
 
 
 //This is a hacky way to quickly do a point turn to 90 degrees.
@@ -46,7 +173,7 @@ await roll(90,50,0.1)
 await roll(90,-50,0.1)
 }
 
-async function stageTwoPart2(){
+async function JadenStage2(){
 
 
 	let setpoint = 120;
@@ -107,7 +234,7 @@ async function stageTwoPart2(){
 
 }
 
-async function stageTwoPart3(){
+async function JadenStage3(){
 
 //This is a hacky way to quickly do a point turn to 90 degrees.
 
@@ -117,7 +244,7 @@ await roll(90,-50,0.1)
 await roll(90,50,0.1)
 }
 
-async function stageTwoPart4(){
+async function JadenStage4(){
 
 	//Travel for eighty centimeters at a heading of 90 degrees
 
